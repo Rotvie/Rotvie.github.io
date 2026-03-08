090ba08c3bdaa389b6eae5fa14bae524a6cbcda2
Rotvie <rotvie@gmail.com>
2026-03-08 14:36:14 +0000

    Deploying to gh-pages from @ Rotvie/Rotvie.github.io@bbd51c21f3aa697cef55e690f116cd57fd8f126c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-introduction-to-sampling-algorithms",
+        },{id: "post-introduction-to-sampling-algorithms",
         
           title: "Introduction to Sampling Algorithms",
         
